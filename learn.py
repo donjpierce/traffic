b7b98f4d601221cbd7b6bbe253c0af31e8b0700a
donjpierce <don.j.pierc@gmail.com>
2019-01-09 23:45:31 -0500

    Added new plot

--- learn.py
+++ learn.py
@@ -61,6 +61,7 @@ for i in range(num_episodes):
         model.fit(np.identity(10)[state:state + 1], target_vec.reshape(-1, 2), epochs=1, verbose=0)
         state = new_s
         r_sum += r
+        print('Action: {}, Reward: {}'.format(action, r))
     r_avg_list.append(r_sum / num_episodes)
 
 
